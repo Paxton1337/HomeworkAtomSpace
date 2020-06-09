80ca37b65b750d0bac959db5e6e02b0b148da4fd
Gleb <azrieldevpoper@gmail.com>
2020-06-09 17:31:47 +0300

    Add last homework

---
+++ Work with API/main.js
@@ -0,0 +1,22 @@
+(async () => {
+  const title = await fetch("https://ghibliapi.herokuapp.com/films", {
+    method: "GET",
+  });
+  
+  const titleJson = await title.json();
+  console.log(titleJson);
+  const mainContainerEl = document.querySelector("#main-container");
+  titleJson.forEach((anime) => {
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    span.innerHTML= `Director: ${anime.director} // `;
+    div.append(span);
+    const span2 = document.createElement("span");
+    span2.innerHTML= `Title: ${anime.title} // `;
+    div.append(span2);
+    const span3 = document.createElement("span");
+    span3.innerHTML= `Release date: ${anime.release_date} `;
+    div.append(span3);
+    mainContainerEl.append(div);
+  });
+})();
