15a2915dd4edd5e4453a0118b31117896ccef5b2
Gleb <azrieldevpoper@gmail.com>
2020-06-24 15:11:26 +0300

    add rewritten homework main chapter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,48 +1,51 @@
-import React, { Component } from "react";
-import ToDoItem from "./ToDoItem/ToDoItem";
-import todosData from "./todosData";
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todoItems: todosData,
-    };
-  }
-
-  handleChange = (id) => {
-    const index = this.state.todoItems.map((item) => item.id).indexOf(id);
-    this.setState((state) => {
-      let { todoItems } = state;
-      todoItems[index].completed = true;
-      return todoItems;
-    });
+import React, { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [Todos, setTodos] = useState([]);
+  const [TodoText, setTodoText] = useState("");
+  const handleAddTodo = () => {
+    if (TodoText) {
+      setTodos([...Todos, { text: TodoText, done: false }]);
+      setTodoText("");
+    }
   };
-
-  render() {
-    const { todoItems } = this.state;
-    const activeTasks = todoItems.filter((task) => task.completed === false);
-    const completedTasks = todoItems.filter((task) => task.completed === true);
-    const finalTasks = [...activeTasks, ...completedTasks].map((item) => {
-      return (
-        <ToDoItem
-          key={item.id}
-          description={item.description}
-          completed={item.completed}
-          handleChange={() => {
-            this.handleChange(item.id);
-          }}
-        />
-      );
+  const handleToggleTodo = (i) => {
+    const todoWithout = Todos.filter((val, index) => {
+      return index !== i;
     });
 
-    return (
+    setTodos([...todoWithout, { text: Todos[i].text, done: !Todos[i].done }]);
+  };
+  return (
+    <div className="main-wrapper">
       <div>
-        <h1>Какие планы на день?</h1>
-        {finalTasks}
+        <label>
+          Input some todo!
+          <input
+            placeholder="To do..."
+            value={TodoText}
+            onChange={({ target }) => setTodoText(target.value)}
+          ></input>
+        </label>
       </div>
-    );
-  }
-}
-
+      <button onClick={handleAddTodo}>Add todo!</button>
+      <div className="todos-block">
+        {Todos.map((todo, index) => {
+          return (
+            <div>
+              <span className={todo.done ? "done" : ""}>{todo.text}</span>
+              <button
+                onClick={() => {
+                  handleToggleTodo(index);
+                }}
+              >
+                {todo.done ? "⨯" : "✔"}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default App;
