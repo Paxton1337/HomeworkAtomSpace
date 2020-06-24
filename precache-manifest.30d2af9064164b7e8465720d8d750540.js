27cd41c2492100ed7ddfdc9bfe679c06b60c9b9a
Gleb <azrieldevpoper@gmail.com>
2020-06-24 15:33:31 +0300

    Updates

---
+++ precache-manifest.30d2af9064164b7e8465720d8d750540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7ace28bdb2d92996a0aaf6d71b73e5",
+    "url": "/HomeworkAtomSpace/react-app/index.html"
+  },
+  {
+    "revision": "ed336cd2d557e4856e61",
+    "url": "/HomeworkAtomSpace/react-app/static/css/main.3d036460.chunk.css"
+  },
+  {
+    "revision": "b72406e27b4d439bc860",
+    "url": "/HomeworkAtomSpace/react-app/static/js/2.bd741662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeworkAtomSpace/react-app/static/js/2.bd741662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed336cd2d557e4856e61",
+    "url": "/HomeworkAtomSpace/react-app/static/js/main.6b09b788.chunk.js"
+  },
+  {
+    "revision": "ed0eba17ec3d6463951b",
+    "url": "/HomeworkAtomSpace/react-app/static/js/runtime-main.33085af0.js"
+  }
+]);
